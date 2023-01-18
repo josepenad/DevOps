6a5ea3c77bd0b9b5faf1ac0ed15cadfa0f42eb28
jjpenad <113132086+josepenad@users.noreply.github.com>
2023-01-17 20:56:34 -0500

    Adding sample test

--- tests/unit/sum.test.ts
+++ tests/unit/sum.test.ts
@@ -1,11 +1,8 @@
-import { hello } from '../../src';
-/* eslint-disable no-magic-numbers */
-/* eslint-disable no-undef */
-const var1 = 2;
-const var2 = var1 + var1;
-describe('sum module', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    expect(var1 + var1).toBe(var2);
+import { hello, sample } from '../../src';
+
+describe('test sample', () => {
+  test('should return Hola Mundo', () => {
+    expect(sample('Mundo')).toBe('HolaMundo');
   });
 });
 
